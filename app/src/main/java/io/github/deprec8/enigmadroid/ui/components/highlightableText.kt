f3373a2d36ec7458d87da1af1e53af4bd65cac7b
deprec8 <deprec8@tutamail.com>
2025-07-18 17:12:15 +0200

    Add HighlightedText composable for text highlighting functionality

---
+++ app/src/main/java/io/github/deprec8/enigmadroid/ui/components/highlightableText.kt
@@ -0,0 +1,68 @@
+/*
+ * Copyright (C) 2025 deprec8
+ *
+ * This file is part of EnigmaDroid.
+ *
+ * EnigmaDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EnigmaDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EnigmaDroid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.deprec8.enigmadroid.ui.components
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.text.AnnotatedString
+import androidx.compose.ui.text.SpanStyle
+import androidx.compose.ui.text.buildAnnotatedString
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.text.withStyle
+
+@Composable
+fun HighlightedText(
+    text: String,
+    maxLines: Int = Int.MAX_VALUE,
+    overflow: TextOverflow = TextOverflow.Clip,
+    highlightedWords: List<String>
+) {
+    val annotatedString = if (highlightedWords.isNotEmpty()) {
+        buildAnnotatedString {
+            var index = 0
+            val regex = highlightedWords.joinToString("|", "(", ")") { Regex.escape(it) }
+                .toRegex(RegexOption.IGNORE_CASE)
+            regex.findAll(text).forEach { matchResult ->
+                val start = matchResult.range.first
+                val end = matchResult.range.last + 1
+                if (index < start) append(text.substring(index, start))
+                withStyle(
+                    SpanStyle(
+                        color = MaterialTheme.colorScheme.primary,
+                        fontWeight = FontWeight.Bold
+                    )
+                ) {
+                    append(text.substring(start, end))
+                }
+                index = end
+            }
+            if (index < text.length) append(text.substring(index))
+        }
+    } else {
+        AnnotatedString(text)
+    }
+    Text(
+        text = annotatedString,
+        maxLines = maxLines,
+        overflow = overflow
+    )
+}
