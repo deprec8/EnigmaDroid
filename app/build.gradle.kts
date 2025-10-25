ebe9295bebfb8eb62be79336f312f676da83325c
deprec8 <deprec8@tutamail.com>
2025-10-25 16:18:27 +0200

    Upgrade dependencies and use Compose BOM
    - Updates Room to version 2.8.3.
    - Upgrades various other dependencies.
    - Adopts the Compose Bill of Materials (BOM) to manage Compose library versions.
    - Reorganizes dependencies for better clarity.

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -131,9 +131,10 @@ dependencies {
 
     // Material
     implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material3.adaptive)
 
     // Lifecycle
-    implementation(libs.androidx.lifecycle.runtime.compose.android)
+    implementation(libs.androidx.lifecycle.runtime.compose)
 
     // Navigation
     implementation(libs.androidx.navigation.compose)
@@ -142,18 +143,13 @@ dependencies {
     implementation(libs.mikepenz.aboutlibraries.core)
     implementation(libs.mikepenz.aboutlibraries.compose.m3)
 
-    // Other
-    implementation(libs.androidx.compose.material.icons.extended)
-    implementation(libs.androidx.core.ktx)
-    implementation(libs.androidx.activity.compose)
+    // Compose
     implementation(platform(libs.androidx.compose.bom))
     implementation(libs.androidx.compose.ui)
-    implementation(libs.androidx.browser)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
     debugImplementation(libs.androidx.compose.ui.tooling)
-    implementation(libs.androidx.compose.material3.adaptive.android)
-    implementation(libs.androidx.adaptive.layout)
+    implementation(libs.androidx.compose.material.icons.extended)
 
     // Ktor
     implementation(libs.ktor.client.core)
@@ -166,6 +162,11 @@ dependencies {
     implementation(libs.androidx.room.runtime)
     implementation(libs.androidx.room.ktx)
     ksp(libs.androidx.room.compiler)
+
+    // Other
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.androidx.activity.compose)
+    implementation(libs.androidx.browser)
 
 }
 
