b929ab7a4682bb439dd07a0b17abe130c3d5e1be
deprec8 <deprec8@tutamail.com>
2026-01-20 17:22:39 +0100

    Add BouquetMenu and EpgContent composables
    Introduces two new reusable composables:
    
    - `EpgContent`: A lazy vertical grid for displaying EPG events. It supports showing channel names, highlighting text, and actions like adding timers or reminders.
    - `BouquetMenu`: A dropdown menu component that allows users to select from a list of bouquets.
    
    Additionally, new string resources for the bouquet menu have been added.

---
+++ app/src/main/java/io/github/deprec8/enigmadroid/ui/epg/components/epgContent.kt
@@ -0,0 +1,124 @@
+/*
+ * Copyright (C) 2025 deprec8
+ *
+ * This file is part of EnigmaDroid.
+ *
+ * EnigmaDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EnigmaDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EnigmaDroid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.deprec8.enigmadroid.ui.epg.components
+
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.consumeWindowInsets
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.imePadding
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.foundation.lazy.grid.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.AddAlert
+import androidx.compose.material.icons.filled.Timer
+import androidx.compose.material.icons.outlined.AddAlert
+import androidx.compose.material.icons.outlined.Timer
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.unit.dp
+import io.github.deprec8.enigmadroid.R
+import io.github.deprec8.enigmadroid.model.api.Event
+import io.github.deprec8.enigmadroid.model.menu.MenuItem
+import io.github.deprec8.enigmadroid.model.menu.MenuSection
+import io.github.deprec8.enigmadroid.ui.components.ContentListItem
+import io.github.deprec8.enigmadroid.ui.components.NoResults
+import io.github.deprec8.enigmadroid.utils.IntentUtils
+import io.github.deprec8.enigmadroid.utils.TimestampUtils
+import kotlinx.coroutines.launch
+
+@Composable
+fun EpgContent(
+    events: List<Event>,
+    paddingValues: PaddingValues,
+    showChannelName: Boolean = false,
+    highlightedWords: List<String> = emptyList(),
+    onAddTimer: (Event) -> Unit,
+) {
+    val scope = rememberCoroutineScope()
+    val context = LocalContext.current
+
+    if (events.isNotEmpty()) {
+        LazyVerticalGrid(
+            columns = GridCells.Adaptive(310.dp),
+            Modifier
+                .fillMaxSize()
+                .consumeWindowInsets(paddingValues)
+                .imePadding(),
+            contentPadding = paddingValues
+        ) {
+            items(events) { event ->
+                ContentListItem(
+                    highlightedWords = highlightedWords,
+                    headlineText = event.title,
+                    supportingText = TimestampUtils.formatApiTimestampToDate(event.beginTimestamp),
+                    overlineText = if (event.beginTimestamp * 1000 <= System.currentTimeMillis()) {
+                        stringResource(R.string.now)
+                    } else {
+                        TimestampUtils.formatApiTimestampToTime(
+                            event.beginTimestamp
+                        )
+                    } + if (showChannelName) {
+                        " - " + event.serviceName
+                    } else {
+                        ""
+                    },
+                    shortDescription = event.shortDescription,
+                    longDescription = event.longDescription,
+                    menuSections = if (event.beginTimestamp * 1000 > System.currentTimeMillis()) {
+                        listOf(
+                            MenuSection(
+                                listOf(
+                                    MenuItem(
+                                        text = stringResource(R.string.add_timer),
+                                        outlinedIcon = Icons.Outlined.Timer,
+                                        filledIcon = Icons.Filled.Timer,
+                                        action = {
+                                            onAddTimer(event)
+                                        }), MenuItem(
+                                        text = stringResource(R.string.add_reminder),
+                                        outlinedIcon = Icons.Outlined.AddAlert,
+                                        filledIcon = Icons.Filled.AddAlert,
+                                        action = {
+                                            scope.launch {
+                                                IntentUtils.addReminder(context, event)
+                                            }
+                                        })
+                                )
+                            )
+                        )
+                    } else {
+                        null
+                    }
+                )
+            }
+        }
+    } else {
+        NoResults(
+            Modifier
+                .consumeWindowInsets(paddingValues)
+                .padding(paddingValues)
+        )
+    }
+}
