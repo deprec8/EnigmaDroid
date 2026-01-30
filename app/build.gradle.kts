ca61ab8efcbc5d86e41cde14d9148043b465fb75
deprec8 <deprec8@tutamail.com>
2026-01-30 17:55:53 +0100

    Bump version and include changelog

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -36,8 +36,8 @@ android {
         applicationId = "io.github.deprec8.enigmadroid"
         minSdk = 28
         targetSdk = 36
-        versionCode = 13
-        versionName = "1.7.0"
+        versionCode = 14
+        versionName = "1.7.1"
 
         vectorDrawables {
             useSupportLibrary = true
