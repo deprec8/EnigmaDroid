e35fd28bd6da142f0dd28d935c511065c2842c6f
deprec8 <deprec8@tutamail.com>
2026-02-16 18:00:10 +0100

    Add pip pre api 31 utility function

---
+++ app/src/main/java/io/github/deprec8/enigmadroid/ui/stream/components/pipListenerPreAPI31.kt
@@ -0,0 +1,54 @@
+/*
+ * Copyright (C) 2025-2026 deprec8
+ *
+ * This file is part of EnigmaDroid.
+ *
+ * EnigmaDroid is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * EnigmaDroid is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with EnigmaDroid.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+package io.github.deprec8.enigmadroid.ui.stream.components
+
+import android.app.PictureInPictureParams
+import android.os.Build
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.DisposableEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.rememberUpdatedState
+import androidx.compose.ui.platform.LocalContext
+
+@Composable
+fun PipListenerPreAPI31(shouldEnterPipMode: Boolean) {
+    val currentShouldEnterPipMode by rememberUpdatedState(newValue = shouldEnterPipMode)
+
+    if (Build.VERSION.SDK_INT < Build.VERSION_CODES.S) {
+        val context = LocalContext.current
+
+        DisposableEffect(context) {
+            val onUserLeaveBehavior = Runnable {
+                if (currentShouldEnterPipMode) {
+                    context.findActivity()
+                        .enterPictureInPictureMode(PictureInPictureParams.Builder().build())
+                }
+            }
+            context.findActivity().addOnUserLeaveHintListener(
+                onUserLeaveBehavior
+            )
+            onDispose {
+                context.findActivity().removeOnUserLeaveHintListener(
+                    onUserLeaveBehavior
+                )
+            }
+        }
+    }
+}
