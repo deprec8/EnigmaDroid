44ba5cde1568b04ee8d619b525f555189420969e
deprec8 <deprec8@tutamail.com>
2026-04-11 14:44:55 +0200

    Remove unused Jetpack Compose state imports in `moviesActionBar.kt`
    This removes unused `getValue`, `setValue`, `mutableStateOf`, and `rememberSaveable` imports from `moviesActionBar.kt`.

--- app/src/main/java/io/github/deprec8/enigmadroid/ui/movies/components/moviesActionBar.kt
+++ app/src/main/java/io/github/deprec8/enigmadroid/ui/movies/components/moviesActionBar.kt
@@ -25,10 +25,6 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.OutlinedCard
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.unit.dp
